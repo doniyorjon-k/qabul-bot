e771f40dfd3d58c8fa7da7296538b3583c8043be
doniyorjon-k <doniyorkuchkarov@gmail.com>
2026-05-26 13:17:06 +0500

    Landing: navbar logoси qabulim.png ga almashtirildi

--- landing/src/components/Nav.jsx
+++ landing/src/components/Nav.jsx
@@ -28,7 +28,7 @@ export default function Nav() {
       <div className="container">
         <div className="nav-inner">
           <a href="#" className="nav-logo">
-            <img src="/logo.png" alt="Qabulim" className="nav-logo-img" />
+            <img src="/qabulim.png" alt="Qabulim" className="nav-logo-img" />
           </a>
           <div className="nav-links">
             {links.map((l) => (
